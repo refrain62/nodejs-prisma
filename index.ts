3e3fa764e70db9a3ea92d9a99e17e7fe9369b414
nao <nao_h13@hotmail.com>
2023-03-23 19:47:40 +0900

    ユーザ情報の登録 - 1 とりあえず登録処理の追加＆REST Clientでテストする設定追加

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
@@ -12,5 +14,16 @@ app.get('/users', async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
   return res.json(users);
 })
+
+app.post('/users', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  return res.json(user);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
