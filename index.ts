ee76e1f06311f1e382ec8cf0ef1073b3a7820f98
nao <nao_h13@hotmail.com>
2023-03-23 19:33:45 +0900

    ユーザ情報の取得 http://localhost:3000/users

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
+import { PrismaClient } from '@prisma/client';
 import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
 
+const prisma = new PrismaClient();
+
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  return res.json(users);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
