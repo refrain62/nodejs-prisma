d128db116365fc5579ba14cd7d33b38a68282aca
nao <nao_h13@hotmail.com>
2023-03-23 19:53:50 +0900

    例外発生時に内容がわかるように調整

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import express, { Request, Response } from 'express';
 
 const app = express();
@@ -26,6 +26,11 @@ app.post('/users', async (req: Request, res: Response) => {
     });
     return res.json(user);
   } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === 'P2002') {
+        console.log('There is a unique constraint violation, a new user cannot be created with this email');
+      }
+    }
     return res.status(400).json(e);
   }
 });
